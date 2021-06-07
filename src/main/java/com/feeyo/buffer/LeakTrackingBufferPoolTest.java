ca8db79495dbfd50854cb9ffc9d7cd3808d6d7cd
zhuam <zhuam@foxmail.com>
2021-06-07 10:35:52 +0800

    rt

--- src/main/java/com/feeyo/buffer/LeakTrackingBufferPoolTest.java
+++ src/main/java/com/feeyo/buffer/LeakTrackingBufferPoolTest.java
@@ -7,31 +7,24 @@ import com.feeyo.buffer.bucket.BucketBufferPool;
 public class LeakTrackingBufferPoolTest {
 	
 	public static void main(String[] args) {
-		
 		//
-		BufferPool bufferPool = new BucketBufferPool(1024 * 2, 1024 * 5,new int[] { 512, 1024 });
-		
+		BufferPool bufferPool = new BucketBufferPool(1024 * 2, 1024 * 100,new int[] { 512, 1024 });
 		LeakTrackingBufferPool x = new LeakTrackingBufferPool(bufferPool);
 		
 		for(int i=0; i< 10; i++) {
 			ByteBuffer buf = x.allocate(888);
 			System.out.println( buf );
-			
+			//
 			x.recycle(buf);
 			x.recycle(buf);
 			x.recycle(buf);
-			
-			System.out.println("xxxxxxxxxx=" + i);
+			System.out.println("buf = " + i);
 		}
-		
-		
+		//
 		System.out.println("BufferPool - leaked acquires:" + x.getLeakedAcquires());
 	    System.out.println("BufferPool - leaked releases:"+ x.getLeakedReleases());
 	    System.out.println("BufferPool - unreleased: " + x.getLeakedResources());
 	        
 	}
-	
-	
-	
 
 }
